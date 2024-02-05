4f94be5cc34dd16ed3acbb0e9bc14742611b9653
Lawunn <lawunnkhaing.tgi@gmail.com>
2024-02-05 22:53:26 +0200

    Added Backend

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,3 +1,35 @@
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const cors = require('cors');
+// const bodyParser = require('body-parser');
+// const contact_router = require('./routes/contactRoutes');
+
+// const app = express();
+
+// const port = process.env.PORT || 3000;
+// app.use(cors());
+// app.use(contact_router);
+
+// // Hardcoded MongoDB connection string
+// const mongoURI = 'mongodb+srv://Lawunn:BlackPink1@cluster.1qhaeok.mongodb.net/Contact';
+
+// mongoose.connect(mongoURI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// db.once('open', () => {
+//   console.log('Connected to MongoDB!');
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
+
+require('dotenv').config();
+const dotenv = require('dotenv');
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -10,8 +42,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(contact_router);
 
-// Hardcoded MongoDB connection string
-const mongoURI = 'mongodb+srv://Lawunn:BlackPink1@cluster.1qhaeok.mongodb.net/Contact';
+const mongoURI = process.env.MONGODB_URI || 'fallback_connection_string';
 
 mongoose.connect(mongoURI, {
   useNewUrlParser: true,
@@ -27,4 +58,3 @@ db.once('open', () => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
